5af82f1c0bf4cc62d4ad2724b796b57c7488ce38
dhivya-eswaran-25 <e.dhivya74@gmail.com>
2026-07-20 15:43:18 +0530

    Update InventoryPage.ts

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -2,10 +2,12 @@ import { Page, Locator,expect } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class InventoryPage extends BasePage {
+  readonly title:Locator;
   readonly checkoutButton: Locator;
   
   constructor(page: Page) {
     super(page);
+    this.title=page.locator('.title');
     this.checkoutButton = page.locator('[data-test="checkout"]');
   }
   async expectLoggedIn(){
