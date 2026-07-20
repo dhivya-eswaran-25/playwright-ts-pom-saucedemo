9925635a53d045f7cdf70981d8aaba041bca3f57
dhivya-eswaran-25 <e.dhivya74@gmail.com>
2026-07-20 15:56:36 +0530

    Update InventoryPage.ts

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -1,19 +1,31 @@
 import { Page, Locator,expect } from '@playwright/test';
 import { BasePage } from './BasePage';
+import { CartPage } from './CartPage';
 
 export class InventoryPage extends BasePage {
   readonly title:Locator;
-  readonly checkoutButton: Locator;
+  readonly firstItemAddtButton: Locator;
+  readonly cartIcon: Locator;
+ // readonly checkoutButton: Locator;
   
   constructor(page: Page) {
     super(page);
     this.title=page.locator('.title');
-    this.checkoutButton = page.locator('[data-test="checkout"]');
+    this.firstItemAddtButton = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
+    this.cartIcon=page.locator('.shopping_cart_link');
+    //this.checkoutButton = page.locator('[data-test="checkout"]');
   }
   async expectLoggedIn(){
     await expect(this.title).toHaveText('Products');
   }
-  async checkout() {
+ /* async checkout() {
     await this.checkoutButton.click();
+  }*/
+  async addFirstItemToCart() {
+    await this.firstItemAddButton.click();
+  }
+  async goToCartt():Promise<CartPage> {
+    await this.cartIcon.click();
+    return new CartPage(this.page);
   }
 }
